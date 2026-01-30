39d501f39dc4b45d6583d8c36ff845e00ac83858
Danniel Libor <dlibor.dev@gmail.com>
2026-01-30 15:32:21 +0800

    feat(routes): add user profile and settings routes with redirection

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -1,11 +1,43 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::view('/', 'welcome');
 
 Route::get('/hello', function () {
     return 'Hello, World!';
+});
+
+Route::prefix('users')->name('users.')->group(function () {
+    // /users/profile/edit
+    Route::get('profile/edit', function () {
+        return 'Profile Edit Page';
+    })->name('profile.edit');
+
+    Route::get('profile/show/{id?}', function ($id = 0) {
+        return 'Profile Show Page: ' . $id;
+    })->name('profile.show');
+
+    // /users/settings
+    Route::get('settings', function () {
+        return 'User Settings Page';
+    })->name('settings');
+});
+
+Route::redirect('/from', '/users/settings')->name('redirect.to.settings');
+
+// Route::fallback(function () {
+//     return '404 Not Found - The requested page does not exist.';
+// });
+
+Route::get('/form', function (Request $request) {
+    $name = $request->name;
+
+    $birthDate = $request->date('birthdate');
+
+    dd($birthDate->diffForHumans());
 });
