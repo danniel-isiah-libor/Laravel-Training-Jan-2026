b00eefe9ed84797a4c913db142c87c150be11ca1
notlath <qltpagsuguiron@tip.edu.ph>
2026-01-31 13:38:08 +0800

    Laravel Lesson - Custom Slots: DONE

--- playground/resources/views/register.blade.php
+++ playground/resources/views/register.blade.php
@@ -1,4 +1,10 @@
-<x-guest-layout>
+<x-guest-layout title="Register Page">
+    <x-slot:header>
+        <h1>This is a header</h1>
+    </x-slot:header>
     <x-page-title title="Register Page" subtitle="Register your account now." />
     <x-input-field :firstName="$first_name" :lastName="$last_name" />
+    <x-slot:footer>
+        <h1>This is a footer</h1>
+    </x-slot:footer>
 </x-guest-layout>
