11eebf4d5569b2e4ac130e50f2b56b9ab92cd462
Danniel Libor <dlibor.dev@gmail.com>
2026-01-31 17:20:09 +0800

    feat(validation): implement UserStoreRequest for user registration validation and add custom PasswordRule

--- playground/app/Http/Controllers/UserController.php
+++ playground/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserStoreRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
@@ -51,37 +52,10 @@ class UserController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
-        $request->validate([
-            'name' => [
-                'required',
-                'string',
-                'max:255'
-            ],
-            'email' => [
-                'required',
-                'string',
-                'email:dns,rfc',
-                'max:255',
-                'unique:users'
-            ],
-            'password' => [
-                'string',
-                'confirmed',
-                Password::min(8)
-                    ->max(12)
-                    ->required()
-                    ->letters()
-                    ->numbers()
-                    ->symbols()
-                    ->mixedCase()
-                    ->uncompromised()
-            ]
-        ]);
+        $validatedForm = $request->validated();
 
-        $name = $request->name;
-        $email = $request->email;
-        $password = $request->password;
+        dd($validatedForm);
     }
 }
