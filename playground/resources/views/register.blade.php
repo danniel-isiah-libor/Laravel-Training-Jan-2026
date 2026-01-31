49d2a3351118769ba4c914b03815f1ea8256cc12
Danniel Libor <dlibor.dev@gmail.com>
2026-01-31 17:57:17 +0800

    feat(errors): add custom 503 error page feat(register): update header and footer text for registration page

--- playground/resources/views/register.blade.php
+++ playground/resources/views/register.blade.php
@@ -6,7 +6,7 @@
                     alt="Your Company" class="mx-auto h-10 w-auto dark:hidden" />
                 <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
                     alt="Your Company" class="mx-auto h-10 w-auto not-dark:hidden" />
-                <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">Sign in
+                <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">Register
                     to your account</h2>
             </div>
         </x-slot:header>
@@ -31,10 +31,9 @@
             </form>
 
             <p class="mt-10 text-center text-sm/6 text-gray-500 dark:text-gray-400">
-                Not a member?
+                Already a member?
                 <a href="#"
-                    class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Start
-                    a 14 day free trial</a>
+                    class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Login</a>
             </p>
         </div>
     </div>
