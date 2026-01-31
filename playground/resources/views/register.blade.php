47a725de46b7da84b32ce26cc6a4b39e2fbb42ed
notlath <qltpagsuguiron@tip.edu.ph>
2026-01-31 13:25:32 +0800

    Laravel Lesson - Slots: DONE

--- playground/resources/views/register.blade.php
+++ playground/resources/views/register.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
-</head>
-
-<body>
+<x-guest-layout>
     <x-page-title title="Register Page" subtitle="Register your account now." />
-    <x-alert />
     <x-input-field :firstName="$first_name" :lastName="$last_name" />
-</body>
-
-</html>
+</x-guest-layout>
