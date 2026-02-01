b89f8320a93a3f69d23382d83d6069cb4dfc546c
froxro <froxro@gmail.com>
2026-02-01 18:14:53 +0800

    Fix Post model factory body generation to return a single paragraph

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+     use HasFactory;
     /** @use HasFactory<\Database\Factories\PostFactory> */
     protected $fillable = [
         'user_id',
